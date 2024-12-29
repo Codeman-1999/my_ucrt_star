41d8c41abdb751faa9b6418c941d3ad4ecabe4c3
Codeman-1999 <qianzhongxin19@163.com>
2024-12-29 23:04:23 +0800

    1.02

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
-SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
+#!/bin/bash
+SHELL_FOLDER=$(cd "$(dirname "$0")"; pwd)
 
 $SHELL_FOLDER/output/qemu/bin/qemu-system-riscv64 \
 -M quard-star \
 -m 1G \
 -smp 8 \
+-nographic \
 # -bios none \
