3f25faaa1471e06863a7c420382924c76b644862
Codeman-1999 <qianzhongxin19@163.com>
2025-02-09 00:11:41 +0800

    16.开启调试功能

--- os/src/main.c
+++ os/src/main.c
@@ -1,19 +1,27 @@
 #include <timeros/os.h>
 #include <timeros/assert.h>
-extern void frame_allocator_test();
+extern void frame_alloctor_init();
+extern void kvminit();
+extern void kvminithart();
 void os_main()
 {
    printk("hello timer os!\n");
-   frame_allocator_test();
-   while (1)
-   {
-      /* code */
-   }
-//    trap_init();
 
-//    task_init();
+   // 内存分配器初始化
+   frame_alloctor_init();
 
-//    timer_init();
+
+   kvminit();
+
    
-//    run_first_task();
+  // kvminithart();
+
+   trap_init();
+
+   task_init();
+
+   timer_init();
+   
+   run_first_task();
+
 }
