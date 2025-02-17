242839362af8f5fee2dd1e71c2330cee6d269363
Codeman-1999 <qianzhongxin19@163.com>
2025-02-17 15:12:59 +0800

    22.sys-fork的实现

--- os/user/write.c
+++ os/user/write.c
@@ -1,13 +1,15 @@
 #include <timeros/types.h>
 #include <timeros/syscall.h>
 #include <timeros/string.h>
+
 int main()
 {
-
+    int pid = sys_fork();
     while (1)
     {
-        char data = getchar();
-        printf("%c",data);
+        // printf("pid:%d\n",pid);
+        // char data = getchar();
+        //printf("write:%d\n",4);
     }
     return 0;
 }
